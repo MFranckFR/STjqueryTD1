998842fee2307b22870fea3608d4a5b0707814ba
MFranckFR <76939469+MFranckFR@users.noreply.github.com>
2021-01-11 10:00:14 +0100

    Premiere démo de jQuery

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#btnIntro").click(function(){
+        console.log("Hello World! ");
+    })
+});
+//$(document).ready(function(){..});
+//$(function(){$("#btnIntro","body").click(function(){console.log('Hello bis')})});
