e2d5b8858af427ec3ead5c886dc9bfe56e5c66e5
MFranckFR <76939469+MFranckFR@users.noreply.github.com>
2021-01-11 11:32:51 +0100

    Swap text et bouton

--- js/main.js
+++ js/main.js
@@ -6,6 +6,17 @@ $(document).ready(function(){
     $("#btnIntro").click(function(){
         $("#q1 div.resultat p.resultat").text(
             $("#q1 div.resultat p.resultat").text() == 'Hello' ? '' : 'Hello'); 
+    });
+
+    $("#btnIntro2").click(function(){
+        // flip/flop button
+        if($("#q2 div.resultat p.resultat").text() == 'Hello'){
+            $("#q2 div.resultat p.resultat").text('');
+            $(this).val('Montre le Hello')
+        }else{
+            $("#q2 div.resultat p.resultat").text('Hello');
+            $(this).val('Cache le Hello');
+        }
     })
 });
 //$(document).ready(function(){..});
